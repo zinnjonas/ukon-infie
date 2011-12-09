7c1cb84dccc43a9c2a5acb676b687deb2f0ce6de
Stephan Heidinger <pandora@mint>
2011-12-09 17:40:13 +0100

    Bug fix for lastPage (formerly shown as ?? in pdf). Also added ukon-test.tex which is an example file. Concurrently added some source files for \loadCpp, \loadJava and \loadSource

---
+++ example/hello_world.cpp
@@ -0,0 +1,10 @@
+// my first program in C++
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Hello World!";
+  return 0;
+}
---
+++ example/hello_world.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+# Hello world python program
+print "Hello World!";
