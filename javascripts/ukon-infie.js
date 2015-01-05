7870995d55d9474345bdbfab49c754ea5fcc4679
Jonas Zinn <Jonas.Zinn@uni-konstanz.de>
2015-01-05 19:14:47 +0100

    First steps to the generator

---
+++ javascripts/ukon-infie.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function(){
+    $("#generate").on("click", function(){
+        
+        var newline;
+        var os = navigator.appVersion;
+        if (os.indexOf("Win")!=-1) newline="\r\n";
+        else if (os.indexOf("Mac")!=-1) newline="\r";
+        else OSName="Linux";
+            
+        
+        var uni = $("#university").val();
+        if( uni == ""){
+            uni = $("#university").attr("placeholder");
+        }
+        
+        var text = "\\documentclass{artcl}"+ newline;
+            text += "test = " + uni + newline;
+        var base = btoa(text);
+        $("#screen").text(text);
+        var download = $('#download');
+        download.removeAttr("Disabled");
+        $(download).attr("download", "test.txt");
+        $(download).attr("href","data:application/octet-stream;charset=utf-8;base64," + base);
+    });
+});
